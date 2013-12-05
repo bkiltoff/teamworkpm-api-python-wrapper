69cf1b7459d91f2408b3ba77b30ae484d373351d
Taylor Pate <taylor@clearcharacter.com>
2013-12-04 19:27:40 -0600

    Moved module files to package, included test that shows example use of the models

---
+++ test.py
@@ -0,0 +1,51 @@
+import json
+from TeamworkPM.api import API
+from TeamworkPM.models import *
+
+api = API("YOUR_COMPANY_NAME", "YOUR_API_KEY")
+COMPANY_ID = 12345					# replace w/ your company ID
+
+people = {}							# TeamworkPM User Ids
+people['User Name'] = 12345
+
+DefaultNotifyList = [ people['User Name'] ]
+
+category = {}
+category['Category 1'] = 1234
+
+def main():
+	projectData = {}
+	projectData['name']        = 'API Test Proj'
+	projectData['description'] = 'Test Project Description'
+	projectData['companyId']   = COMPANY_ID
+	projectData["category-id"] = category['Category 1']
+
+	proj = Project(api)
+	proj.create(projectData)
+
+	for userID in DefaultNotifyList:
+		proj.addPerson(userID)
+
+	personid = people['User Name']
+
+	tasklistTemplates = [ {"name":"Task List Template","todo-list-template-id":123456} ]
+
+	for tasklist in tasklistTemplates:
+		tasklist_id = proj.createTaskList(tasklist)	
+		
+		if tasklist_id:
+			tasklist_obj = TaskList(api,tasklist_id)
+			td = tasklist_obj.getTasks()
+			aa = json.loads(td)
+			for tasks in aa['todo-items']:
+				tk = Task(api,tasks['id'])
+				tk.assignPerson(tasks['id'],personid,tasks['description'])
+	
+	proj.createMessage( {"title":"Project Message","body":"Job Start Message", "notify":DefaultNotifyList} )
+	
+main()
+
+
+
+
+
